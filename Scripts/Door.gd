b880301d7872826930437fdba8bbe3b875d72067
Domar <omerserifoduncu@gmail.com>
2025-12-04 21:10:13 +0300

    biraz güzelleştirme

--- Scripts/Door.gd
+++ Scripts/Door.gd
@@ -5,10 +5,18 @@ extends StaticBody2D
 @onready var interior = $/root/Game/InteriorTileMap
 @onready var player = $/root/Game/Player
 @onready var sprite = $AnimatedSprite2D
+var isInterior = true
 
 
 func _physics_process(delta:float) -> void:
 	#iceri disari cikma
+	if isInterior:
+		if exterior.modulate.a > 0 or interior.modulate.a < 1:
+			get_tree().create_tween().tween_property(interior,"modulate",Color.WHITE,0.1)
+			get_tree().create_tween().tween_property(exterior,"modulate",Color.TRANSPARENT,0.1)
+	elif interior.modulate.a > 0 or exterior.modulate.a < 1:
+		get_tree().create_tween().tween_property(exterior,"modulate",Color.WHITE,0.1)
+		get_tree().create_tween().tween_property(interior,"modulate",Color.TRANSPARENT,0.1)
 	if sprite.animation == "anim":
 		if sprite.frame < 4:
 			$/root/Game/Player/AnimatedSprite2D.scale *= 0.9
@@ -16,8 +24,7 @@ func _physics_process(delta:float) -> void:
 			$/root/Game/Player/AnimatedSprite2D.scale /= 0.9
 		else:
 			$/root/Game/Player/AnimatedSprite2D.scale = Vector2.ONE #nolur nolmaz
-			exterior.visible = not exterior.visible
-			interior.visible = not interior.visible
+			isInterior = not isInterior
 			player.set_collision_layer_value(1, not player.get_collision_layer_value(1))
 			player.set_collision_layer_value(2, not player.get_collision_layer_value(2))
 			player.set_collision_mask_value(1, not player.get_collision_mask_value(1))
