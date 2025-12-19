7d756b5579257b721e13d8c4a9b24afd5cd7aded
Domar <omerserifoduncu@gmail.com>
2025-12-19 10:38:43 +0300

    bu transform businessi anlayınca devam

--- Scripts/InteriorAutoLights.gd
+++ Scripts/InteriorAutoLights.gd
@@ -1,14 +1,19 @@
 extends Node2D
 
+@onready var lightArea = get_parent().get_child(1)
 @onready var tilemap = get_parent()
 @export var LIGHT_LENGTH = 32
 var windows
+var collider = CollisionShape2D.new()
+var colliderShape = ConvexPolygonShape2D.new()
 #var source:TileSetAtlasSource = tile_set.get_source(0) as TileSetAtlasSource
 #var rect = source.get_tile_texture_region(Vector2(1,0))
 #var image:Image = source.get_texture().get_image()
 #var tile_image:Image = image.get_region(rect)
 var tex:ImageTexture
 func _ready() -> void:
+	colliderShape.set_points(PackedVector2Array([Vector2(5,0),Vector2(10,0),Vector2(15,LIGHT_LENGTH),Vector2(5,LIGHT_LENGTH)]))
+	collider.set_shape(colliderShape)
 	windows = tilemap.get_used_cells_by_id(0,0,Vector2(1,0))
 	#for i in tile_image.get_height():
 	#	for i2 in tile_image.get_width():
@@ -19,7 +24,8 @@ func _ready() -> void:
 	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	queue_redraw()
+	#queue_redraw()
+	pass
 
 func _draw() -> void:
 	var start = 16
@@ -28,4 +34,7 @@ func _draw() -> void:
 		for i in range(-start,LIGHT_LENGTH):
 			tex.set_size_override(Vector2(13+(i+start)/2,22))
 			var worldPos = tilemap.map_to_local(vec) + Vector2(-8-(i+start)/4,i)
+			var id = lightArea.create_shape_owner(collider)
+			lightArea.shape_owner_add_shape(id,collider.get_shape())
+			lightArea.shape_owner_set_transform(id, collider.transform)
 			draw_texture(tex,worldPos,Color(1,1,1,0.033))#-((i+start)/(LIGHT_LENGTH+start))*0.033))
