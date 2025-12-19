8369a28e2b5f67fe9a16392ebffc5dd5f112f75a
Domar <omerserifoduncu@gmail.com>
2025-12-19 21:19:50 +0300

    öf

--- Scripts/LightAreaForWindow.gd
+++ Scripts/LightAreaForWindow.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends TileMap
 
 var isPlayerInLight = false
 var lightTimer = 0.0
@@ -6,14 +6,7 @@ var lightTimer = 0.0
 @onready var moon = $/root/Game/Camera2D/Moon
 @onready var sun = $/root/Game/Camera2D/Sun
 
-func _ready() -> void:
-	set_collision_layer_value(get_parent().get_tileset().get_physics_layer_collision_layer(0),true)
-	set_collision_mask_value(get_parent().get_tileset().get_physics_layer_collision_layer(0),true)
-
 func _physics_process(delta: float) -> void:
-	if get_collision_layer_value(get_parent().get_tileset().get_physics_layer_collision_layer(0)) != player.get_collision_layer_value(get_parent().get_tileset().get_physics_layer_collision_layer(0)):
-		isPlayerInLight = false
-		lightTimer = 0.0
 	#$/root/Game/Label.text = "Light Timer: {0}, Is Player In: {1}".format([str(lightTimer),str(isPlayerIn)])
 	if (not player.isInNoLightArea) and isPlayerInLight and player.HEALTH > 0 and sun.position.y < moon.position.y:
 		lightTimer -= delta
